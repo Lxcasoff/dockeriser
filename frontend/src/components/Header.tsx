666d340b24a45fa9e61305ae91e035292e4369c1
Lxcasoff <99267496+Lxcasoff@users.noreply.github.com>
2025-05-05 17:37:54 +0200

    💄 refactor: passage de Tailwind à Bootstrap pour un rendu plus simple et direct

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -19,7 +19,7 @@ export default function Header() {
               <span className="navbar-toggler-icon"></span>
             </button>
   
-            <div className="collapse navbar-collapse" id="navbarNav">
+            <div className="navbar-collapse show" id="navbarNav">
               <ul className="navbar-nav ms-auto">
                 <li className="nav-item">
                   <a className="nav-link active" href="#">
